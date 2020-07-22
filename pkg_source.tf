5f7dddec179e0455a4e79c7daa09130a2362935c
Tim Irvin <irvin@nettempo.com>
2020-07-22 16:57:32 -0700

    Added code to add a random hash to the zipped output directory so a hash change happens when a terraform destroy occurs.  Move the temp zip hash to a build directory.

---
+++ pkg_source.tf
@@ -0,0 +1,39 @@
+resource "random_string" "name" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+data "archive_file" "dir_hash_zip" {
+  type        = "zip"
+  source_dir  = "${var.lambda_source}/"
+  output_path = "${path.module}/build/dir_hash_zip"
+}
+
+resource "null_resource" "install_python_dependencies" {
+  triggers = {
+    requirements = sha1(file("${var.lambda_source}/requirements.txt"))
+    dir_hash     = data.archive_file.dir_hash_zip.output_base64sha256
+    random_string = random_string.name.result
+  }
+
+  provisioner "local-exec" {
+    command = "bash ${path.module}/scripts/py_pkg.sh"
+
+    environment = {
+      source_code_path = var.lambda_source
+      path_cwd         = path.cwd
+      path_module      = path.module
+      runtime          = var.lambda_runtime
+      function_name    = "${var.name}-${var.environment}"
+      random_string    = random_string.name.result
+    }
+  }
+}
+
+data "archive_file" "lambda_zip" {
+  depends_on  = [null_resource.install_python_dependencies]
+  type        = "zip"
+  source_dir  = "${path.cwd}/lambda_pkg_${random_string.name.result}/"
+  output_path = var.lambda_output_path
+}
--- scripts/py_pkg.sh
+++ scripts/py_pkg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-#set directory permissions
+
 if ! pip install --upgrade virtualenv; then
   echo "Unable to install "virtualenv" package" 1>&2
   exit 1
@@ -8,7 +8,6 @@ cd $path_cwd
 dir_name=lambda_pkg_$random_string/
 mkdir -p $dir_name
 
-#virtual env setup
 cd $path_module
 if ! virtualenv -p $runtime env-$function_name; then
   echo "Unable to create virtualenv for $runtime, you need to install $runtime on your build system" 1>&2
@@ -20,7 +19,6 @@ if [[ $source_code_path != /* ]];then
    source_code_path=$path_cwd/$source_code_path
 fi
 
-#installing python dependencies
 FILE=$source_code_path/requirements.txt
 if [ -f $FILE ]; then
   echo "requirements.txt file exists in source_code_path. Installing dependencies.."
@@ -28,15 +26,10 @@ if [ -f $FILE ]; then
 else
   echo "requirements.txt file does not exist. Skipping installation of dependencies."
 fi
-#deactivate virtualenv
 deactivate
-#creating deployment package
-#cd env-$function_name/lib/$runtime/site-packages/
-#cp -r . $path_cwd/$dir_name
 cp -r $source_code_path/. $path_cwd/$dir_name
-#removing virtual env folder
+echo "Random value to trigger a source rebuild "$random_string > $path_cwd/$dir_name/.hashtrigger
 rm -rf $path_module/env-$function_name/
-#add lambda_pkg directory to .gitignore
 GIT_FILE=$path_cwd/.gitignore
 grep -q '#ignore lambda_pkg dir' $path_cwd/.gitignore 2> /dev/null || echo '#ignore lambda_pkg dir' >> $path_cwd/.gitignore
 grep -q $dir_name $path_cwd/.gitignore 2> /dev/null || echo $dir_name >> $path_cwd/.gitignore
