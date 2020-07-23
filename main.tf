cf7f7dd9756267c4331f7c57488a0cfe61b48636
Tim Irvin <irvin@nettempo.com>
2020-07-22 18:32:18 -0700

    Moved providers to the module

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
+provider "archive" {
+  version = "~> 1.3"
+}
+
+provider "random" {
+  version = "~> 2.3"
+}
+
 locals {
   lambda_source      = var.lambda_source == "" ? "${path.root}/lambda_source" : var.lambda_source
   lambda_output_path = var.lambda_output_path == "" ? "${path.root}/${var.name}-${var.environment}.zip" : var.lambda_output_path
