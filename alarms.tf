fa7e2590a473df770d1f3370a49a87bb189b571b
Tim Irvin <irvin@nettempo.com>
2020-07-24 10:21:47 -0700

    Set CloudWatch logs retention time to 30 days

--- alarms.tf
+++ alarms.tf
@@ -1,5 +1,6 @@
 resource "aws_cloudwatch_log_group" "this" {
   name = "/aws/lambda/${aws_lambda_function.this.function_name}"
+  retention_in_days = 30
   tags = merge(
     local.tags,
     var.global_tags,
