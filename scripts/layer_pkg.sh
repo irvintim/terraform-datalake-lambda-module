3aa9e1e928436619d5a777ed58177bf96fd9a7b8
Tim Irvin <irvin@nettempo.com>
2020-07-30 15:41:44 -0700

    Testing values in lambda layer config to try to get around the error "ValidationException"

--- scripts/layer_pkg.sh
+++ scripts/layer_pkg.sh
@@ -3,6 +3,10 @@
 cd $path_cwd
 FILE=$source_code_path/requirements.txt
 # If requirements.txt hasn't changed then do nothing
+echo unzip -p $layer_zipfile requirements.txt
+unzip -p $layer_zipfile requirements.txt
+echo $FILE
+cat $FILE
 if unzip -p $layer_zipfile requirements.txt | diff -b -B  -q - $FILE; then
   exit 0
 fi
