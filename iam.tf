0c17bd0048bb2959e7e808d7262d933f7ae6a87b
Tim Irvin <irvin@nettempo.com>
2021-03-08 15:52:36 -0800

    Added * to SSM Parameter path

--- iam.tf
+++ iam.tf
@@ -103,7 +103,7 @@ data "aws_iam_policy_document" "this" {
     ]
     resources = [
       "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter${var.ssm_config_path}",
-      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter${var.ssm_config_path}/*"
+      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter${var.ssm_config_path}*"
     ]
   }
 }
